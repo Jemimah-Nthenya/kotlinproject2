8d056d07dffee4dc8572ac2dd3a63ad18d61addf
Jemimah-Nthenya <jemimahogonda@gmail.com>
2024-04-05 22:21:06 +0300

    add files

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,28 @@
-fun main() {
-    val contacts= Contacts(9808762,"Janet","0114556672")
-    println(contacts.id)
-    println(contacts.)
+fun main(){
+val people=People("Ken",7903564,"0114567435")
+    println(people.name)
+    println(people.id)
+    println(people.phoneNumber)
+
+val myPeople=People("Jane",7377665,"0712632961")
+    println(myPeople.name)
+    println(myPeople.id)
+    println(myPeople.phoneNumber)
+
+val person=People("Maryam",5762526,"0764783857")
+    println(person.name)
+    println(person.id)
+    println(person.phoneNumber)
+
+val person2=People("Mary",6573412,"0110572835")
+    println(people.name)
+    println(people.id)
+    println(people.phoneNumber)
 }
-data class Contacts(
-        var id: Int? = null,
-        var name: String,
-        var number: String
-)
+open class People(var name:String, var id:Int, var phoneNumber:String){}
+
+
+
 
 
 
