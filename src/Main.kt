a17bda1089cd6a972b9e6464d5540614148a2c4c
Jemimah-Nthenya <jemimahogonda@gmail.com>
2024-04-05 08:30:47 +0300

    add files

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,14 @@
-fun Entity(tableName :String)
-class Contacts (
-    var id : Int?=null,
-    var name : String,
-    var number : String
+fun main() {
+    val contacts= Contacts(9808762,"Janet","0114556672")
+    println(contacts.id)
+}
+data class Contacts(
+        var id: Int? = null,
+        var name: String,
+        var number: String
 )
+
+
 
 
 
